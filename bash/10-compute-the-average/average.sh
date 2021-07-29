a8c8725bb2e1f17d49be9ad20a6e6d10ccd3ad26
Xander Harris <plebis@hotmail.com>
2021-07-29 15:32:19 -0700

    Get a sum of items
    See #10

--- bash/10-compute-the-average/average.sh
+++ bash/10-compute-the-average/average.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-count=0
+divisor=0
 input=input/input00.txt
 sum=0
 
 while read -r line;
 do
-    if [[ $count == 0 ]];
+    if [[ $divisor == 0 ]];
     then
         divisor=$line
     else
-        
+        sum=$((sum + line))
     fi
 done < $input
+
+printf "${sum}"
