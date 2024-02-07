4f499640c6be77e92a4e176379c8bf839ca12000
Xander Harris <xandertheharris@gmail.com>
2024-02-07 14:05:02 -0800

    python basic certificate
    See #8
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- python/python-basic-cert/reverse-words-swap-cases/reverse_and_swap.py
+++ python/python-basic-cert/reverse-words-swap-cases/reverse_and_swap.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+"""This module reverses case and words in a sentence."""
 
 def reverse_words_order_and_swap_cases(sentence):
+    """Reverse the case and order words in a sentence."""
     # Write your code here
     ret_val = str()
     s_case_swapped = sentence.swapcase()
@@ -8,3 +11,9 @@ def reverse_words_order_and_swap_cases(sentence):
     ret_val = " ".join(s_list)
     # print(ret_val)
     return ret_val
+
+
+if __name__ == '__main__':
+    TEST_STR = "This is a regular sentence."
+    RESULT = reverse_words_order_and_swap_cases(TEST_STR)
+    print(RESULT)
