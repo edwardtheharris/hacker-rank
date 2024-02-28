9cf171d6affb2ef773ac5056043435bd7c1db34f
Xander Harris <xandertheharris@gmail.com>
2024-02-28 12:03:36 -0800

    See #12
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- bash/cut-1/cut-1.sh
+++ bash/cut-1/cut-1.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Specify the file path
-input_file="/dev/stdin"
-# Set n to 3 to extract the 3rd character
-n=3
-
-# Read each line from the file
-while IFS= read -r line
-do
-  # Extract the 3rd character from the line
-  # Bash indexing starts at 0, but we use n-1 here because we're extracting based on user expectation (1-based indexing)
-  echo "${line:n-1:1}"
-done < "$input_file"
+# shellcheck disable=SC2162
+while read line_txt; do
+    echo "${line_txt}" | cut -c 3
+done
