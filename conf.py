f152b42b5d1ccf6c2bc06452d75a2de1078c8235
Xander Harris <xandertheharris@gmail.com>
2025-10-26 10:31:50 -0700

    Fix python syntax, update pre-commit
    Closes #239

--- conf.py
+++ conf.py
@@ -6,55 +6,56 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 """
+
 # pylint: disable=invalid-name,redefined-builtin
 
 import os
 import sys
 
-sys.path.append(os.path.abspath('python/python-basic-cert/reverse_and_swap'))
-sys.path.append(os.path.abspath('python/python-basic-cert/implement_multiset'))
-sys.path.append(os.path.abspath('python/simple_array_sum'))
-sys.path.append(os.path.abspath('python/compare_the_triplets'))
+sys.path.append(os.path.abspath("python/python-basic-cert/reverse_and_swap"))
+sys.path.append(os.path.abspath("python/python-basic-cert/implement_multiset"))
+sys.path.append(os.path.abspath("python/simple_array_sum"))
+sys.path.append(os.path.abspath("python/compare_the_triplets"))
 
-author = 'Xander Harris'
+author = "Xander Harris"
 autoyaml_root = "."
 autoyaml_doc_delimiter = "###"
 autoyaml_comment = "#"
 autoyaml_level = 10
 autoyaml_safe_loader = True
-copyright = '2024, Xander Harris'
+copyright = "2024, Xander Harris"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-    '.venv/*',
-    '.tmp/*',
-    '.pytest_cache/*',
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    ".venv/*",
+    ".tmp/*",
+    ".pytest_cache/*",
 ]
 
 extensions = [
-    'myst_parser',
-    'sphinx_design',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.intersphinx',
-    'sphinxcontrib.autoyaml',
-    'sphinxcontrib.youtube',
+    "myst_parser",
+    "sphinx_design",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.autoyaml",
+    "sphinxcontrib.youtube",
 ]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_logo = '_static/img/hr-logo.svg'
-html_favicon = '_static/img/hr-logo.svg'
-html_static_path = ['_static']
-html_theme = 'sphinx_book_theme'
-myst_dmath_double_inline=True
+html_logo = "_static/img/hr-logo.svg"
+html_favicon = "_static/img/hr-logo.svg"
+html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
+myst_dmath_double_inline = True
 myst_enable_extensions = [
     "amsmath",
     "attrs_block",
@@ -73,14 +74,11 @@ myst_enable_extensions = [
     "tasklist",
 ]
 myst_title_to_header = True
-project = 'Hacker Rank Challenges'
+project = "Hacker Rank Challenges"
 rst_epilog = """
 .. sectionauthor:: Xander Harris <xandertheharris@gmail.com>
 """
-release = '0.0.1'
-show_authors=True
-source_suffix = {
-    '.md': 'markdown',
-    '.rst': 'restructuredtext'
-}
-templates_path = ['_templates']
+release = "0.0.1"
+show_authors = True
+source_suffix = {".md": "markdown", ".rst": "restructuredtext"}
+templates_path = ["_templates"]
